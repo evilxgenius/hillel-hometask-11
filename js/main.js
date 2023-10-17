26cfb5651ae5c7d05404f91fc5fafbe56888ac50
Sergey E <evilx.emelyanov@gmail.com>
2023-10-17 20:38:20 +0300

    change let to const for initArray

--- js/main.js
+++ js/main.js
@@ -37,7 +37,7 @@ function nullifyArray(array, ...except) {
 }
 
 (() => {
-    let initArray = [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
+    const initArray = [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
     const positiveElements = positiveElementsOf(initArray);
     const negativeElements = negativeElementsOf(initArray);
     const evenPositiveElements = evenElementsOf(positiveElements);
